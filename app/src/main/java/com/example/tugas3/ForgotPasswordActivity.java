3b82e4eba6e0fc5fd04c85a073d3e5538152a431
tamabsndra <narotamaaa3@gmail.com>
2022-09-28 20:46:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas3/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
